94e64884c552ced7fc63b119ac97a43b17ea97ef
JRaiden <jraiden16@outlook.com>
2019-01-29 13:19:40 -0600

    Create MenosFirst.java

---
+++ Factory_namer/MenosFirst.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class MenosFirst extends Namer {
+
+    public MenosFirst() {
+    }
+
+    public void metodo(String s) {
+        int i = s.indexOf("-");//find space
+        if (i > 0) {//left is last name
+            first = s.substring(0, i).trim();//right is first name
+            last = s.substring(i + 1).trim();
+        }
+    }
+}
