7ab53bc070c7c433e54110309e62237d8754e916
JRaiden <jraiden16@outlook.com>
2019-01-29 13:21:13 -0600

    Create Namer.java

---
+++ Factory_namer/Namer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class Namer {
+    String last;
+    String first;
+    void print(){
+        System.out.println("first= " + first);
+        System.out.println("last= " + last);
+    }
+    void metodo(String entry){
+        
+    }
+}
